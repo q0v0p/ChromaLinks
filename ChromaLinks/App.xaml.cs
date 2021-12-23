db21191e6894a491f0610ffdd0ee822287d1abcc
Zxlerr <zxlerr@gmail.com>
2021-12-23 22:41:11 +0900

    Initial commit

---
+++ ChromaLinks/App.xaml.cs
@@ -0,0 +1,203 @@
+﻿using ChromaControl.Shared;
+
+using CUESDK;
+
+using Microsoft.Win32;
+
+using NzxtRGB;
+
+using Razer.Chroma.Broadcast;
+
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ChromaLinks
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        private static RzChromaBroadcastAPI _api;
+        private static SmartDeviceV2 _smartDeviceV2;
+        private int _corsairDeviceCount;
+        private Task _syncTask;
+        private bool _isLinkNZXT;
+        private bool _isLinkCorsair;
+        private ContextMenuStrip _menu;
+        private string _nzxtMenuText => _isLinkNZXT ? "NZXTとのリンクを解除する" : "NZXTとリンクする";
+        private string _corsairMenuText => _isLinkCorsair ? "Corsairとのリンクを解除する" : "Corsairとリンクする";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var icon = GetResourceStream(new Uri("icon.ico", UriKind.Relative)).Stream;
+            _menu = new ContextMenuStrip();
+            _menu.Items.Add(_nzxtMenuText, null, NZXTLink_Click);
+            _menu.Items.Add(_corsairMenuText, null, CorsairLink_Click);
+            _menu.Items.Add("終了", null, Exit_Click);
+            var notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Visible = true,
+                Icon = new System.Drawing.Icon(icon),
+                Text = "ChromaLinks",
+                ContextMenuStrip = _menu
+            };
+            notifyIcon.MouseClick += NotifyIcon_Click;
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(systemEvents_SessionSwitch);
+            _syncTask = Task.Factory.StartNew(() => startChromaSync());
+        }
+
+        private void startChromaSync()
+        {
+            if (!Utilities.InitializeEnvironment("ASUS", "DEF05DCE-1662-4D9A-A312-A31028651915"))
+                return;
+
+            // Setup NZXT Device
+            var config = new NzxtDeviceConfig();
+            config.RestartNzxtCamOnFail = true;
+            config.RestartNzxtOnClose = true;
+
+            _smartDeviceV2 = SmartDeviceV2.OpenDeviceAsync(config).Result;
+
+            // Setup Corsair Device
+            CorsairLightingSDK.PerformProtocolHandshake();
+
+            if (CorsairLightingSDK.GetLastError() != CorsairError.Success)
+                Console.WriteLine("Failed to connect to iCUE");
+
+            CorsairLightingSDK.RequestControl(CorsairAccessMode.ExclusiveLightingControl);
+            _corsairDeviceCount = CorsairLightingSDK.GetDeviceCount();
+
+            _api = new RzChromaBroadcastAPI();
+            _api.ConnectionChanged += _api_ConnectionChanged;
+            _api.ColorChanged += _api_ColorChanged;
+
+            try
+            {
+                _api.Init(Guid.Parse(Utilities.ApplicationGuid));
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void _api_ColorChanged(object sender, RzChromaBroadcastColorChangedEventArgs e)
+        {
+            if (_isLinkNZXT)
+            {
+                Color[] channel1Colors = Enumerable.Repeat(e.Colors[0], 16).ToArray();
+                Color[] channel2Colors = Enumerable.Repeat(e.Colors[0], 16).ToArray();
+                _smartDeviceV2.SendRGB(1, channel1Colors);
+                _smartDeviceV2.SendRGB(2, channel2Colors);
+            }
+            if (_isLinkCorsair)
+            {
+                try
+                {
+                    Color[] colors = e.Colors;
+                    for (int i = 0; i < _corsairDeviceCount; i++)
+                    {
+                        CorsairLedPositions deviceLeds = CorsairLightingSDK.GetLedPositionsByDeviceIndex(i);
+                        CorsairLedColor[] buffer = new CorsairLedColor[deviceLeds.NumberOfLeds];
+                        CorsairLedColor corsairLedColor = default(CorsairLedColor);
+                        corsairLedColor.R = colors[0].R;
+                        corsairLedColor.G = colors[0].G;
+                        corsairLedColor.B = colors[0].B;
+                        CorsairLedColor currentColor = corsairLedColor;
+                        for (int j = 0; j < deviceLeds.NumberOfLeds; j++)
+                        {
+                            buffer[j] = currentColor;
+                            buffer[j].LedId = deviceLeds.LedPosition[j].LedId;
+                        }
+                        CorsairLightingSDK.SetLedsColorsBufferByDeviceIndex(i, buffer);
+                        CorsairLightingSDK.SetLedsColorsFlushBuffer();
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void _api_ConnectionChanged(object sender, RzChromaBroadcastConnectionChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void NotifyIcon_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+
+            }
+        }
+
+        private void NZXTLink_Click(object sender, EventArgs e)
+        {
+            if (_isLinkNZXT)
+                _isLinkNZXT = false;
+            else
+                _isLinkNZXT = true;
+
+            _menu.Items[0].Text = _nzxtMenuText;
+        }
+
+        private void CorsairLink_Click(object sender, EventArgs e)
+        {
+            if (_isLinkCorsair)
+                _isLinkCorsair = false;
+            else
+                _isLinkCorsair = true;
+
+            _menu.Items[1].Text = _corsairMenuText;
+        }
+
+        private void systemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            SessionSwitchReason reason = e.Reason;
+            if (reason != SessionSwitchReason.SessionLock)
+                onLed();
+            else
+                offLed();
+        }
+
+        private void onLed()
+        {
+            if (CorsairLightingSDK.GetLastError() != CorsairError.Success)
+                Console.WriteLine("Failed to connect to iCUE");
+
+            Task.Delay(5000).Wait();
+            CorsairLightingSDK.RequestControl(CorsairAccessMode.ExclusiveLightingControl);
+            _corsairDeviceCount = CorsairLightingSDK.GetDeviceCount();
+
+            _isLinkCorsair = true;
+        }
+
+        private void offLed()
+        {
+            Color[] channel1Colors;
+            Color[] channel2Colors = (channel1Colors = new Color[0]);
+            _smartDeviceV2.SendRGB(1, channel1Colors);
+            _smartDeviceV2.SendRGB(2, channel2Colors);
+
+            _isLinkCorsair = false;
+            CorsairLightingSDK.ReleaseControl(CorsairAccessMode.ExclusiveLightingControl);
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            _smartDeviceV2?.Dispose();
+            CorsairLightingSDK.ReleaseControl(CorsairAccessMode.ExclusiveLightingControl);
+            Shutdown();
+        }
+    }
+}
---
+++ ChromaLinks/Utilities.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using System;
+using System.IO;
+
+namespace ChromaControl.Shared
+{
+    /// <summary>
+    /// The utilities class
+    /// </summary>
+    public class Utilities
+    {
+        /// <summary>
+        /// Allocates a console window
+        /// </summary>
+        [DllImport("kernel32")]
+        private static extern void AllocConsole();
+
+        /// <summary>
+        /// The application id
+        /// </summary>
+        internal static string ApplicationId = "Default";
+
+        /// <summary>
+        /// The application guid
+        /// </summary>
+        internal static string ApplicationGuid = "";
+
+        /// <summary>
+        /// Initializes the environment
+        /// </summary>
+        /// <param name="applicationId">The application id</param>
+        /// <param name="applicationGuid">The application id</param>
+        /// <param name="args">The command line arguments</param>
+        /// <returns>If sucessful</returns>
+        public static bool InitializeEnvironment(string applicationId, string applicationGuid)
+        {
+            ApplicationId = applicationId;
+            ApplicationGuid = applicationGuid;
+
+            var mutex = new Mutex(true, applicationId, out var result);
+
+            if (!result)
+            {
+                return false;
+            }
+
+            //if (Debugger.IsAttached || args.Length > 0 && args[0] == "--console")
+            //{
+            //    AllocConsole();
+            //}
+
+            return true;
+        }
+    }
+}
